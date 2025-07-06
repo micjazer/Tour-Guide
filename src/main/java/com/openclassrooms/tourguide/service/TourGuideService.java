a1db0f438f8f12a109971da665c622ea32acc171
Michel Jazeron <michel.jazeron@gmail.com>
2025-07-06 21:47:02 +0200

    C7 : Refonte de la stratégie CI/CD : ajout de la gestion des artefacts, intégration de Docker et optimisation des performances du projet

--- src/main/java/com/openclassrooms/tourguide/service/TourGuideService.java
+++ src/main/java/com/openclassrooms/tourguide/service/TourGuideService.java
@@ -2,7 +2,6 @@ package com.openclassrooms.tourguide.service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
